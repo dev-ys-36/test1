3c40de098165bd9ead5679e3bbbbddc785017292
dev-ys-36 <98698629+dev-ys-36@users.noreply.github.com>
2022-03-05 13:44:55 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+
+        delegate void testFunction();
+
+        public static void Main()
+        {
+            testFunction af = () =>
+            {
+                Console.WriteLine("anonymous function");
+            };
+
+            Func<int> func1 = () => 123;
+
+            Console.WriteLine(func1());
+
+            af();
+            
+            OldMethod();
+        }
+
+        //[Obsolete("legacy method")]
+        public static void OldMethod()
+        {
+            Console.WriteLine("test");
+        }
+
+    }
+}
